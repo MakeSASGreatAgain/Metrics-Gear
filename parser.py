ebbdf55293fc72f5384076099b2405a5ab775b26
egorz734 <egorz734@mail.ru>
2021-05-30 08:44:40 +0300

    DEV: parsing now from a page

--- parser.py
+++ parser.py
@@ -1,16 +1,15 @@
 import pandas as pd
 import pyodbc 
 
-path = 'data.htm'
-with open(path) as f:
-    data = pd.read_html(f)
+path = input()
+data = pd.read_html(path)
 data = data[0]
 
 table1 = data[0:8][[7, 8]].dropna().drop([0, 1])
 
 data1 = data.fillna(0)
 data1 = data1[9:][0]
-names = data1.drop_duplicates().drop([11, 12])
+names = data1.drop_duplicates().drop(10)
 table2 = data[9:].dropna()
 table2[0] = names.values
 
